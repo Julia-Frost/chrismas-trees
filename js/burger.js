8db42c527c2a4343af787d4740926447eeea2c30
Julia-Frost <juliafrostwork@gmail.com>
2022-06-16 16:20:17 +0300

    add changes to mobile menu

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,8 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileMenuIcon = document.querySelector('[data-menu-icon]');
+  const mobileMenuCross = document.querySelector('[data-menu-cross]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
@@ -9,5 +11,7 @@
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('is-open');
+    mobileMenuIcon.classList.toggle('is-open');
+    mobileMenuCross.classList.toggle('is-open');
   });
 })();
